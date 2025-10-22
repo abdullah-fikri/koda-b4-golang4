898dc685c45d1598316712838abbd62730abfd49
abdullah-fikri <abdullahfikri981@gmail.com>
2025-10-22 17:58:24 +0700

    feat: added login feat based on data that has been created in the register

---
+++ auth/login.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func (u *User) Login(data []User) (*User, bool) {
+	fmt.Println("--- Login ---")
+
+	fmt.Print("Enter Your Email: ")
+	fmt.Scan(&u.Email)
+
+	fmt.Print("Enter Your password: ")
+	fmt.Scan(&u.Password)
+
+	found := false
+	for i := range data {
+		if u.Email == data[i].Email && u.Password == data[i].Password {
+			found = true
+			fmt.Println("Login Success, press enter to back...")
+			reader := bufio.NewReader(os.Stdin)
+			_, _ = reader.ReadString('\n')
+			return &data[i], true
+		}
+	}
+	if !found {
+		fmt.Println("Wrong Email Or Password, press enter to restart")
+		u.Login(data)
+	}
+	return nil, false
+}
